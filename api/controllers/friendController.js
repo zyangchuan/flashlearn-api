f0a416cf254f0adb67f52fab840f0694d4622a8a
zyangchuan <zyangchuan1@gmail.com>
2024-04-14 11:45:56 +0800

    feedbacks for friendship feature

--- api/controllers/friendController.js
+++ api/controllers/friendController.js
@@ -1,4 +1,8 @@
 const { Sequelize } = require('../db/sequelize');
+
+// Take away all the unused packages
+// Remember to add the foreign key constraints for the Friendship model in the index.js file
+
 const { BadRequestError, NotFoundError } = require('../errors');
 const notFound = require('../middlewares/not-found');
 const { Friendship, User } = require('../models');
@@ -7,6 +11,8 @@ const { Op } = require('sequelize');
 
 const findFriends = async (req, res) => {
     const query = req.params.id;
+
+    // variable name use searchResult
     const friendsFinder = await User.findAll({
       where: Sequelize.where(
         Sequelize.fn('LOWER', Sequelize.col('username')),
@@ -15,7 +21,9 @@ const findFriends = async (req, res) => {
         }
       )
     });
-  
+
+    // No need to do check and throw error in this case 
+    // cuz empty result is a valid result
     if (friendsFinder.length > 0) {
       res.status(StatusCodes.OK).json({ users: friendsFinder });
     } else {
@@ -36,6 +44,8 @@ const getFriends = async (req, res) => {
     where: {
       requestee: req.user.id,
       accepted: true
+      // Add this line below cuz these are the only attributes we need
+      // attributes: ['requestee', 'requestor]
     }
   });
 
@@ -49,6 +59,7 @@ const getFriends = async (req, res) => {
     }
   });
 
+  // Join the table first so u dont need to query again
   const friends = await User.findAll({
     where: {
       id: friendIds
@@ -58,13 +69,16 @@ const getFriends = async (req, res) => {
   res.status(StatusCodes.OK).json({ friends });
 };
 
+// Use sendFriendRequest for the function name
 const addFriend = async (req, res) => {
-  const  FriendId  = req.params.id;
+  const FriendId = req.params.id;
   Friendship.create({
     requestee: req.user.id,
-    requestor: FriendId,
+    requestor: FriendId, // Why is the requestor the target user
     accepted: false
   })
+
+  // Msg use friend request sent
   res.status(StatusCodes.CREATED).json({ msg: "Friendship created." });
 
 };
@@ -79,6 +93,8 @@ const getFriendRequests = async (req, res) => {
 
   const requestorIds = friendRequests.map(friendRequest => friendRequest.requestor);
 
+
+  // Use join table
   const requestors = await User.findAll({
     where: {
       id: requestorIds
@@ -89,7 +105,7 @@ const getFriendRequests = async (req, res) => {
 };
 
 const acceptFriendRequest = async (req, res) => {
-  const friendId = req.params.id;
+  const friendId = req.params.id; // variable name use requestorId
 
   const friendRequest = await Friendship.findOne({
     where: {
@@ -99,6 +115,9 @@ const acceptFriendRequest = async (req, res) => {
     }
   });
 
+
+  // Use friendRequest.accepted = true
+  //     await friendRequest.save()
   if (friendRequest) {
     await friendRequest.update({
       accepted: true
@@ -106,12 +125,16 @@ const acceptFriendRequest = async (req, res) => {
 
     res.status(StatusCodes.OK).json({ msg: "Friend request accepted successfully." });
   } else {
+
+    // Eh this one must be more specific cannot let the front end guess
+    // Again friend request result can be empty cuz its a valid result so dont need to throw error for this case
+    // So just validate if the friend request has already been accepted and throw error
     res.status(StatusCodes.NOT_FOUND).json({ error: "Friend request not found or already accepted." });
   }
 };
 
 const declineFriendRequest = async (req, res) => {
-  const friendId = req.params.id;
+  const friendId = req.params.id; // Use requestorId
 
   const friendRequest = await Friendship.findOne({
     where: {
@@ -125,10 +148,13 @@ const declineFriendRequest = async (req, res) => {
     await friendRequest.destroy();
     res.status(StatusCodes.OK).json({ msg: "Friend request declined successfully." });
   } else {
+    // Same thing as above
     res.status(StatusCodes.NOT_FOUND).json({ error: "Friend request not found or already accepted." });
   }
 };
 
+
+// This function just find friendship record and delete can already
 const removeFriend = async (req, res) => {
   const friendId = req.params.id;
 
@@ -152,6 +178,8 @@ const removeFriend = async (req, res) => {
   if (deletedFriendshipCount > 0) {
     res.status(StatusCodes.OK).json({ msg: "Friend removed successfully." });
   } else {
+
+    // Throw notFoundError if no record found
     res.status(StatusCodes.NOT_FOUND).json({ error: "Friendship not found." });
   }
 };
