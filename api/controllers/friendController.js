0dd1e828f120dbb7a31e456e7cbab3fd87c41463
j4ryl <165132550+j4ryl@users.noreply.github.com>
2024-04-27 17:50:20 +0800

    Update friendController.js

--- api/controllers/friendController.js
+++ api/controllers/friendController.js
@@ -56,7 +56,7 @@ const getFriends = async (req, res) => {
     where: {
       id: friendIds,
     },
-    attributes: ['username', 'pfp']
+    attributes: ['username']
   });
 
   res.status(StatusCodes.OK).json({ friends });
